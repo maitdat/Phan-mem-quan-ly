bfce06cbec8e39759d580c05d7297109b7891d2e
maitd <maitd2010@gmail.com>
2024-08-24 15:50:56 +0700

    Diem danh buoi 2

---
+++ MVC/FirstMVCDemo/Student/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult MaiTienDat()
+        {
+            return View();
+        }
+        public IActionResult FormInput()
+        {
+            return View();
+        }
+    }
+}
